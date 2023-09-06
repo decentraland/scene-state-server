b772dcbf6e4d1f678165301e026dcc554cd8177e
Gon Pombo <gonzalo@decentraland.org>
2023-09-06 10:31:18 -0300

    feat: start local scene automatically (#18)

--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,7 @@
 import { Lifecycle } from '@well-known-components/interfaces'
 import { setupRouter } from './controllers/routes'
 import { AppComponents, GlobalContext, TestComponents } from './types'
+import { loadOrReload } from './controllers/handlers/debugging-handler'
 
 // this function wires the business logic (adapters & controllers) with the components (ports)
 export async function main(program: Lifecycle.EntryPointParameters<AppComponents | TestComponents>) {
@@ -20,4 +21,10 @@ export async function main(program: Lifecycle.EntryPointParameters<AppComponents
 
   // start ports: db, listeners, synchronizations, etc
   await startComponents()
+
+  // Start scene always if the localScenePath is declared
+  const localPath = await components.config.getString('LOCAL_SCENE_PATH')
+  if (localPath) {
+    await loadOrReload(components, 'localScene')
+  }
 }
