037d859f09f8e74932668bc1c432649ee1461d59
Pravus <pravusjif@gmail.com>
2023-12-13 18:25:38 +0100

    added base sdk6 bridge support, still WIP

--- src/logic/scene-runtime/apis.ts
+++ src/logic/scene-runtime/apis.ts
@@ -1,4 +1,5 @@
 import { serializeCrdtMessages } from './logger'
+import {contentFetchBaseUrl, sdk6FetchComponent, sdk6SceneContent} from "../sceneFetcher";
 
 export const LoadableApis = {
   EnvironmentAPI: {},
@@ -23,6 +24,19 @@ export const LoadableApis = {
   Runtime: {
     getRealm: () => {
       return { realmInfo: { isPreview: false } }
+    },
+    // readFile is needed for the adaption-layer bridge to run SDK6 scenes as an SDK7 scene
+    readFile: async ({ fileName }: { fileName: String }) => {
+      console.log(`PRAVS - Runtime.readFile - 1 - ${fileName}`)
+      // const entryPoint = sdk6SceneContent.find(({ file }: Content) => file === fileName)
+      const fileHash = sdk6SceneContent.find(({ file }: any) => file === fileName).hash
+      const res = await sdk6FetchComponent.fetch(`${contentFetchBaseUrl}${fileHash}`)
+      // console.log(`PRAVS - Runtime.readFile - 2 - ${await res.text()}`)
+      return {
+        // content: new Uint8Array(await res.blob())
+        content: new Uint8Array(await res.text())
+        // content: await res.text()
+      }
     }
   }
 }
--- src/logic/sceneFetcher.ts
+++ src/logic/sceneFetcher.ts
@@ -42,34 +42,39 @@ export async function getGameDataFromLocalScene(scenePath: string): Promise<stri
   return readFileSync(scenePath, 'utf-8')
 }
 
-export async function getGameDataFromRemoteScene(fetch: IFetchComponent, sceneCoords: string): Promise<string> {
-  // TODO: Find out how reliable are these content urls, will they change in the future?
-  
+export let sdk6SceneContent: any
+export const contentFetchBaseUrl = 'https://peer.decentraland.org/content/contents/'
+export let sdk6FetchComponent: any
+export async function getGameDataFromRemoteScene(fetch: IFetchComponent, sceneCoords: string): Promise<string> {  
   // get scene id
   const mappingsUrl = `https://peer.decentraland.org/content/entities/active`
   let fetchResponse = await fetch.fetch(mappingsUrl, {
     method: 'post',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ pointers: [sceneCoords] })
-  })  
+  })
   const sceneData = (await fetchResponse.json())[0]  
   console.log(`PRAVS - scene id:${sceneData.id} - sdk7? ${sceneData.metadata.runtimeVersion === '7'}`)
-  
-  const contentUrl = 'https://peer.decentraland.org/content/contents/'
-  
-  // TODO: get main.crdt and use that instead of main file
-  
-  // get scene main file hash (index.js/game.js)
-  const sceneMainFileHash = sceneData.content.find(($: any) => $.file === sceneData.metadata.main)?.hash  
-  if (!sceneMainFileHash) {
-    throw new Error(`Cannot scene's main asset file hash`)
-  }
 
-  /*if (sceneData.metadata.runtimeVersion !== '7') {
+  // SDK6 scenes support
+  if (sceneData.metadata.runtimeVersion !== '7') {
+    // sdk6 scene content will be later read by the adaption-layer internally using the Runtime.readFile API
+    // sdk6SceneSourceCode = fetchResponse.blob()
+    sdk6SceneContent = sceneData.content
+    sdk6FetchComponent = fetch
+
     fetchResponse = await fetch.fetch(`https://renderer-artifacts.decentraland.org/sdk7-adaption-layer/main/index.js`)
     return await fetchResponse.text()
-  }*/
+  }
   
-  fetchResponse = await fetch.fetch(`${contentUrl}${sceneMainFileHash}`)  
+  // TODO: get main.crdt and use that instead of main file to support editor-made scenes
+  
+  // get scene main file hash (index.js/game.js)
+  const sceneMainFileHash = sceneData.content.find(($: any) => $.file === sceneData.metadata.main)?.hash
+  if (!sceneMainFileHash) {
+    throw new Error(`Cannot find scene's main asset file hash`)
+  }
+  fetchResponse = await fetch.fetch(`${contentFetchBaseUrl}${sceneMainFileHash}`)
+  
   return await fetchResponse.text()
 }
